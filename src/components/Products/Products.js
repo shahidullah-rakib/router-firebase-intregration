276d8721fbc0e4930ecf44f2046b7a9be56933e8
shsidulla <shsidullarakib@gmail.com>
2022-07-06 12:19:15 +0600

    custom hook

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import useFirebase from '../../hooks/useFirebase';
 
-const Home = () => {
+const Products = () => {
     const { user } = useFirebase();
     return (
         <div>
-            <h4>This is home</h4>
+            <h2>Knock Knock who is there</h2>
             <p>Corrent User Is: {user ? user.displayName : "No One sigh-in"}</p>
+
         </div>
     );
 };
 
-export default Home;
+export default Products;
